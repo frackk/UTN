b451ff71c2612ad9842d35679b90200d325be4f9
Frackk <franco.abianxpg@gmail.com>
2024-04-20 16:39:42 -0300

    added unfinished ejerc. 7

--- Ejercicio_7/Ejercicio_7.cpp
+++ Ejercicio_7/Ejercicio_7.cpp
@@ -1,9 +1,80 @@
+// TERMINAR
+
 #include <iostream>
 using namespace std;
 
 int main () {
 
-    cout << "Test GitHub" << endl;
+    int a, b, c;
+    string triangulo;
+    cout << "Ingrese 3 longitudes" << endl;
+    cin >> a >> b >> c;
+
+    if (a!=b) {
+
+        if (a!=c) {
+        
+            triangulo = "Triangulo Escaleno";
+            cout << triangulo << endl;
+        
+        }
+        else {
+
+            triangulo = "Triangulo Isoseles";
+            cout << triangulo << endl;
+
+        }
+    }
+    else {
+
+        if (a!=c) {
+
+            triangulo = "Triangulo Isoseles";
+            cout << triangulo << endl;
+
+        }
+        else {
+
+            triangulo = "Triangulo Equilatero";
+            cout << triangulo << endl;
+
+        }
+
+    }
+    if (b!=c) {
+
+        if (c!=a) {
+        
+            triangulo = "Triangulo Escaleno";
+            cout << triangulo << endl;
+
+        }
+        else {
+
+            triangulo = "Triangulo Isoseles";
+            cout << triangulo << endl;
+
+        }
+
+    } 
+    else {
+
+        if (b!=a) {
+
+            triangulo = "Triangulo Isoseles";
+            cout << triangulo << endl;
+
+        }
+        else {
+
+            triangulo = "Triangulo Equilatero";
+            cout << triangulo << endl;
+
+        }
+
+    }
+
+    // cout << a << b << c << endl;
 
     system("pause");
     return 0;
