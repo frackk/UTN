38cf3e55b6165807b90bc9a9c5242e818d98ff5a
Frackk <franco.abianxpg@gmail.com>
2024-04-20 19:48:03 -0300

    commented 7 (unfinished)

--- Ejercicio_7_unfinished/Ejercicio_7.cpp
+++ Ejercicio_7_unfinished/Ejercicio_7.cpp
@@ -10,14 +10,18 @@ int main () {
     cout << "Ingrese 3 longitudes" << endl;
     cin >> a >> b >> c;
 
+    // si a es distinto de b
     if (a!=b) {
 
+        // Y a es distinto de c
         if (a!=c) {
         
             triangulo = "Triangulo Escaleno";
             cout << triangulo << endl;
         
         }
+
+        // PERO es igual a c
         else {
 
             triangulo = "Triangulo Isoseles";
@@ -25,14 +29,19 @@ int main () {
 
         }
     }
+
+    // O si a es igual a b
     else {
 
+        // PERO distinto de c
         if (a!=c) {
 
             triangulo = "Triangulo Isoseles";
             cout << triangulo << endl;
 
         }
+
+        // Y TAMBIEN igual a c
         else {
 
             triangulo = "Triangulo Equilatero";
@@ -41,8 +50,11 @@ int main () {
         }
 
     }
+
+    // si b es dintito de c
     if (b!=c) {
 
+        
         if (c!=a) {
         
             triangulo = "Triangulo Escaleno";
